d8c7be15de353e4271571046e4e541fe4898bac3
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2025-02-12 01:17:23 +0530

    Updated

---
+++ Day-2/Feb7th-java-packages/pack1/Test3.java
@@ -0,0 +1,6 @@
+package pack3; // package statement has to be the first statement in java code (PICS)
+public class Test1{
+    public static void main(String[] args) {
+        System.out.print("Pack3 -> test1.java");
+    }
+}
---
+++ Day-2/Feb7th-java-packages/pack1/subpack/Test1.java
@@ -0,0 +1,6 @@
+package subpack; // package statement has to be the first statement in java code (PICS)
+public class Test1{
+    public static void main(String[] args) {
+        System.out.println("Sub pack-1 -> test1.java");
+    }
+}

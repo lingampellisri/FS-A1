267db9f7e7428d8f2e4f5d28f946c54826eec9c4
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2025-02-12 01:16:52 +0530

    Updated Day-4

--- Day-2/Feb7th-java-packages/pack1/Test1.java
+++
@@ -1,6 +0,0 @@
-package pack1; // package statement has to be the first statement in java code (PICS)
-public class Test1{
-    public static void main(String[] args) {
-        System.out.println("Pack1 -> test1.java");
-    }
-}

516e2153023777abed855711d2d3fbe1d48629b6
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2025-03-02 02:13:53 +0530

    Updated using dfs

--- Day-14/DistinctShapes.java
+++ Day-14/DistinctShapes.java
@@ -51,6 +51,61 @@ Sample Output-2:
 4
 
  */
+import java.util.*;
 public class DistinctShapes {
-    
+
+    private static final int[] dx = {0,-1,0,1}; 
+    private static final int[] dy = {1,0,-1,0};
+
+    private static void dfs(int row,int col,int baserow,int basecol,int[][] grid,boolean[][] visited,int m,int n,List<String> shape){
+
+        shape.add((row - baserow) + "," + (col - basecol));
+        visited[row][col] = true;
+
+        for(int i = 0 ; i < 4 ; i++){
+            int nx = row + dx[i];
+            int ny = col + dy[i];
+
+            if(nx >= 0 && nx < m && ny >=0 && ny < n && !visited[nx][ny] && grid[nx][ny] == 1){
+                dfs(nx,ny,baserow,basecol,grid,visited,m,n,shape);
+            }
+        }
+    }
+    private static int countDistinctIslands(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        HashSet<String> uniqueIslands = new HashSet<>();
+        boolean[][] visited = new boolean[m][n];
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(!visited[i][j] && grid[i][j] == 1){
+                    List<String> shape = new ArrayList<>();
+                    dfs(i,j,i,j,grid,visited,m,n,shape);
+                    // Collections.sort(shape); 
+                    uniqueIslands.add(String.join(";", shape));
+                }
+            }
+        }
+
+        return uniqueIslands.size();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] grid = new int[m][n];
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                grid[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.print(countDistinctIslands(grid));
+        
+        sc.close();
+    }
 }

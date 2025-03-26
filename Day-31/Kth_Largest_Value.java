0af9b47befbe1bf5acffad872662efae32a1c68f
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2025-03-27 00:29:15 +0530

    Updated using heap sort

--- Day-31/Kth_Largest_Value.java
+++ Day-31/Kth_Largest_Value.java
@@ -34,177 +34,154 @@ Sample output-2:
 3
  */
 import java.util.*;
-interface Heap{
-    void insert(int val); // insert into Heap
-    int poll(); // delete the top element either max element in max heap or min element in min heap
-    void display();
-    
-}
+class Solution {
+    private static int ans;
 
-class MaxHeap implements Heap{
-    private int[] heap;
-    int size;
-
-    public MaxHeap(int maxSize){
-        this.size = 0;
-        heap = new int[maxSize+1];
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 
-    private void swap(int i, int j){
-        if (i == j) return;
-        int temp = heap[i];
-        heap[i] = heap[j];
-        heap[j] = temp;
+    private static void heapify(int[] arr, int n, int i) {
+        // T.C:- log(n)
+
+        int curr = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] >= arr[curr]) {
+            curr = left;
+        }
+
+        if (right < n && arr[right] >= arr[curr]) {
+            curr = right;
+        }
+
+        if (curr != i) {
+            swap(arr, curr, i);
+            // now node i has got placed at correct position now it's time for placing curr
+            heapify(arr, n, curr);
+        }
     }
-    
 
-    @Override
-    public void insert(int val){
-        size++;
-        heap[size] = val;
+    private static void heapify(int[] arr) {
+        int n = arr.length;
+        // heapify is used to build a maxHeap from this array
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i); // place this node at's it's correct place
+        }
+    }
 
-        // now move this node to it's correct place
-        int i = size;
-        while(i > 1){
-            int parent = i / 2;
-            if(heap[i] > heap[parent]){
-                swap(i,parent);
-                i = parent;
-            }else{
+    private static void heapSort(int[] arr, int n, int k) {
+        // T.C:- O(nlogn)
+        // if we sort a maxHeap we get result in ascending order
+        // convert array to max heap first
+        heapify(arr);
+
+        int heapSize = n;
+        int cnt = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            // first swap first and last
+            cnt++;
+            swap(arr, 0, i);
+            if (cnt == k) {
+                ans = arr[i]; // once reached k stop
                 return;
             }
-        }
-    }
-
-    @Override
-    public int poll(){
-        // replace the first element with last element
-        int maxEle = heap[1];
-        heap[1] = heap[size];
-
-        // decrement the size
-        size--;
-
-        // Now move root node to it's correct place
-        int i = 1;
-        while(i < size){
-            int left = 2 * i;
-            int right = 2 * i + 1;
-
-            if(left <= size && heap[left] > heap[i]){
-                swap(i,left);
-                i = left;
-            }else if(right <= size && heap[right] > heap[i]){
-                swap(i,right);
-                i = right;
-            }else{
-                return maxEle;
-            }
+            heapSize--;
+            heapify(arr, heapSize, 0); // alaway heapify the root node
         }
 
-        return maxEle;
     }
 
-    @Override
-    public void display(){
-        for(int i = 1 ; i <= size ; i++){
-            System.out.print(heap[size] + " ");
-        }
+    private static int findKthLargest(int[] nums, int k) {
+        // may be heap sort could help us
+        ans = 0;
+        int n = nums.length;
+        heapSort(nums, n, k);
+        return ans;
     }
 
-    public int KthLargestElement(int k){
-        // pop element k times 
-        int cnt = 0;
-        while(cnt < k - 1){
-            poll();
-            cnt++;
-
-        }
-
-        return poll();
-
-    }
-}
-public class Kth_Largest_Value {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int n = sc.nextInt();
         int p = sc.nextInt();
-        MaxHeap maxheap = new MaxHeap(n);
 
-        for(int i = 0 ; i < n ; i++){
-            maxheap.insert(sc.nextInt());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        System.out.println(maxheap.KthLargestElement(p));
+        System.out.println(findKthLargest(arr,p));
         sc.close();
 
     }
 }
 
 /*
-import java.util.*;
-class TreapNode
-{
-    int data;
-    int priority;
-    TreapNode left;
-    TreapNode right;
-    TreapNode(int data)
-    {
-        this.data = data;
-        this.priority = new Random().nextInt(1000);
-        this.left = this.right = null;
-    }
-}
-
-class KthLargest
-{
-    static int k;
-    public static TreapNode rotateLeft(TreapNode root)
-    {
-       
-    }
-
-    public static TreapNode rotateRight(TreapNode root)
-    {
-        
-    }
-
-    public static TreapNode insertNode(TreapNode root, int data){
-        
-    }
-
-    static void inorder(TreapNode root)
-    {
-        
-    }
-
-// 	static void printTreap(TreapNode root)
-//     {
-//         if (root == null)
-//             return;
-//         printTreap(root.left);
-//         System.out.println(root.data + " " + root.priority);
-//         printTreap(root.right);
-//     }
-    public static void main(String[] args)
-	{
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int p = sc.nextInt();
-        k=n-p+1;
-        int arr[] = new int[n];
-        for(int i=0;i<n;i++){
-            arr[i] = sc.nextInt();
-        }
-        TreapNode root = null;
-        for(int a:arr){
-            root = insertNode(root,a);
-        }
-// 		printTreap(root);
-        inorder(root);
-    }
-}
+ * import java.util.*;
+ * class TreapNode
+ * {
+ * int data;
+ * int priority;
+ * TreapNode left;
+ * TreapNode right;
+ * TreapNode(int data)
+ * {
+ * this.data = data;
+ * this.priority = new Random().nextInt(1000);
+ * this.left = this.right = null;
+ * }
+ * }
+ * 
+ * class KthLargest
+ * {
+ * static int k;
+ * public static TreapNode rotateLeft(TreapNode root)
+ * {
+ * 
+ * }
+ * 
+ * public static TreapNode rotateRight(TreapNode root)
+ * {
+ * 
+ * }
+ * 
+ * public static TreapNode insertNode(TreapNode root, int data){
+ * 
+ * }
+ * 
+ * static void inorder(TreapNode root)
+ * {
+ * 
+ * }
+ * 
+ * // static void printTreap(TreapNode root)
+ * // {
+ * // if (root == null)
+ * // return;
+ * // printTreap(root.left);
+ * // System.out.println(root.data + " " + root.priority);
+ * // printTreap(root.right);
+ * // }
+ * public static void main(String[] args)
+ * {
+ * Scanner sc = new Scanner(System.in);
+ * int n = sc.nextInt();
+ * int p = sc.nextInt();
+ * k=n-p+1;
+ * int arr[] = new int[n];
+ * for(int i=0;i<n;i++){
+ * arr[i] = sc.nextInt();
+ * }
+ * TreapNode root = null;
+ * for(int a:arr){
+ * root = insertNode(root,a);
+ * }
+ * // printTreap(root);
+ * inorder(root);
+ * }
+ * }
  */

853d7f78e2079f07a8d1e225329db0d26b62adf0
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2025-04-20 19:38:30 +0530

    Updated

---
+++ Day-38-Syncrhonization/LongestCommonPrefix.java
@@ -0,0 +1,38 @@
+/*
+Mr.Uday is trying to develop a software which reads array of strings from user and gives the
+longest common prefix of those strings, if no common prefix then it will give empty string. 
+Help uday to develop program.
+
+
+Input Format: 
+-------------
+Line-1: array of strings seperated by ','
+
+Output Foramt: 
+--------------
+longest common prefix.
+
+Sample Input-1:
+---------------
+flower,flow,flight
+
+Sample Output-1: 
+----------------
+fl
+
+Sample Input-2:
+--------------
+dog,racecar,car
+
+Sample Output-2:
+----------------
+""
+
+Explanation: There is no common prefix among the input strings.
+
+Note: USE TRIE DATASTRUCTURE
+      STRING SHOULD CONTAIN ONLY LOWER CASE ALPHABETS(a to z  only)
+ */
+public class LongestCommonPrefix {
+    
+}
